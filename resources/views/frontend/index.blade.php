e51f9bf8b78f6992dc3cedaccd48befc4e7d8015
Kevin Kipngeno <35192411+kipngenokevin@users.noreply.github.com>
2021-05-10 11:53:37 +0300

    First commit

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,36 @@
+<html lang="en">
+<head>
+  <title>Internship Program</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+</head>
+<body>
+
+
+  <div class="sidebar">
+    <div class="sidebar-brand">
+      <h1><span class="lab la-accusoft"></span>Internship</h1>
+    </div>
+    <div class="sidebar-menu">
+      <ul>
+        <li><a href=""><span class="las la-igloo"></span>
+          <span>Dashboard</span></a>
+        </li>
+        <li><a href=""><span class="las la-igloo"></span>
+          <span>Dashboard</span></a>
+        </li>
+        <li><a href=""><span class="las la-igloo"></span>
+          <span>Dashboard</span></a>
+        </li>
+        <li><a href=""><span class="las la-igloo"></span>
+          <span>Dashboard</span></a>
+        </li>
+        <li><a href=""><span class="las la-igloo"></span>
+          <span>Dashboard</span></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+</body>
+</html>
